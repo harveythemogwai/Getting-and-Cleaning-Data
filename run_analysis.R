f6d28d639b8ee09ea04125ffa2539bdec2dcaf91
harveythemogwai <tignor.stephen@gmail.com>
2014-05-25 16:31:30 -0400

    collapsed code

--- run_analysis.R
+++ run_analysis.R
@@ -12,10 +12,12 @@ y_train <- read.table("y_train.txt")
 subject_train <- read.table("subject_train.txt")
 ## Binds data frames and selects measures only with mean and standard deviation
 test <- cbind(X_test, subject_test)
-subset_test <- c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,345:350,424:429,503:504,516:517,529:530,542:543)
+subset_test <- c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,
+                 253:254,266:271,345:350,424:429,503:504,516:517,529:530,542:543)
 subset_test <- test[subset_test]
 train <- cbind(X_train, subject_train)
-subset_train <- c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,345:350,424:429,503:504,516:517,529:530,542:543)
+subset_train <- c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,
+                  253:254,266:271,345:350,424:429,503:504,516:517,529:530,542:543)
 subset_train <- train[subset_train]
 t_set <- rbind(subset_test,subset_train)
 actions <- rbind(y_test, y_train)
@@ -30,4 +32,20 @@ actions$V1[actions$V1 == 6] <- "Laying"
 tidyset <- cbind(t_set, actions)
 library(data.table)
 ## Set data frame column names
-setnames(tidyset, c('tbodyaccmeanx','tbodyaccmeany','tbodyaccmeanz','tbodyaccstdx','tbodyaccstdy','tbodyaccstdz','tgravityaccmeanx','tgravityaccmeany','tgravityaccmeanz','tgravityaccstdx','tgravityaccstdy','tgravityaccstdz','tbodyaccjerkmeanx','tbodyaccjerkmeany','tbodyaccjerkmeanz','tbodyaccjerkstdx','tbodyaccjerkstdy','tbodyaccjerkstdz','tbodygyromeanx','tbodygyromeany','tbodygyrostdz','tbodygyrojerkmeanx','tbodygyrojerkmeany','tbodygyrojerkmeanz','tbodygyrojerkstdx','tbodygyrojerkstdy','tbodygyrojerkstdz','tbodyaccmagmean','tbodyaccmagstd','tgravityaccmagmean','tgravityaccmagstd','tbodyaccjerkmagmean','tbodyaccjerkmagstd','tbodygyromagmean','tbodygyromagstd','tbodygyrojerkmagmean','tbodygyrojerkmagstd','fbodyaccmeanx','fbodyaccmeany','fbodyaccmeanz','fbodyaccstdx','fbodyaccstdy','fbodyaccstdz','fbodyaccjerkmeanx','fbodyaccjerkmeany','fbodyaccjerkmeanz','fbodyaccjerkstdx','fbodyaccjerkstdy','fbodyaccjerkstdz','fbodygyromeanx','fbodygyromeany','fbodygyromeanz','fbodygyrostdx','fbodygyrostdy','fbodygyrostdz','fbodyaccmagmean','fbodyaccmagstd','fbodybodyaccjerkmagmean','fbodybodyaccjerkmagstd','fbodybodygyromagmean','fbodybodygyromagstd','fbodybodygyrojerkmagmean','fbodybodygyrojerkmagstd','fbodybodygyromagmean','fbodybodygyromagstd','fbodybodygyrojerkmagmean','activity'))
+setnames(tidyset, c('tbodyaccmeanx','tbodyaccmeany','tbodyaccmeanz','tbodyaccstdx','tbodyaccstdy',
+                    'tbodyaccstdz','tgravityaccmeanx','tgravityaccmeany','tgravityaccmeanz',
+                    'tgravityaccstdx','tgravityaccstdy','tgravityaccstdz','tbodyaccjerkmeanx',
+                    'tbodyaccjerkmeany','tbodyaccjerkmeanz','tbodyaccjerkstdx','tbodyaccjerkstdy',
+                    'tbodyaccjerkstdz','tbodygyromeanx','tbodygyromeany','tbodygyrostdz',
+                    'tbodygyrojerkmeanx','tbodygyrojerkmeany','tbodygyrojerkmeanz','tbodygyrojerkstdx',
+                    'tbodygyrojerkstdy','tbodygyrojerkstdz','tbodyaccmagmean','tbodyaccmagstd',
+                    'tgravityaccmagmean','tgravityaccmagstd','tbodyaccjerkmagmean','tbodyaccjerkmagstd',
+                    'tbodygyromagmean','tbodygyromagstd','tbodygyrojerkmagmean','tbodygyrojerkmagstd',
+                    'fbodyaccmeanx','fbodyaccmeany','fbodyaccmeanz','fbodyaccstdx','fbodyaccstdy',
+                    'fbodyaccstdz','fbodyaccjerkmeanx','fbodyaccjerkmeany','fbodyaccjerkmeanz',
+                    'fbodyaccjerkstdx','fbodyaccjerkstdy','fbodyaccjerkstdz','fbodygyromeanx',
+                    'fbodygyromeany','fbodygyromeanz','fbodygyrostdx','fbodygyrostdy','fbodygyrostdz'
+                    ,'fbodyaccmagmean','fbodyaccmagstd','fbodybodyaccjerkmagmean','fbodybodyaccjerkmagstd'
+                    ,'fbodybodygyromagmean','fbodybodygyromagstd','fbodybodygyrojerkmagmean',
+                    'fbodybodygyrojerkmagstd','fbodybodygyromagmean','fbodybodygyromagstd',
+                    'fbodybodygyrojerkmagmean','activity'))
